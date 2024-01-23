1e0daecd656d47e3faf9c27e8a4eee2c53db5a2f
DeviPriya <kanulladevipriya@gmail.com>
2024-01-23 10:45:28 +0530

    big num

---
+++ greater.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Enter first number:"
+read Num1
+sleep 2s
+echo "Enter second number:"
+read Num2
+if [ $Num1 -gt $Num2 ]
+then 
+echo "First Nuber $Num1 is greater than Second Number $Num2"
+else
+echo "Second  Number $Num2 is greater than First Number $Num1"
+fi
