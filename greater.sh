6425f4d2005de2688abef91750fcfbb5e5e0485c
DeviPriya <kanulladevipriya@gmail.com>
2024-01-23 10:47:25 +0530

    added elif

--- greater.sh
+++ greater.sh
@@ -7,6 +7,9 @@ read Num2
 if [ $Num1 -gt $Num2 ]
 then 
 echo "First Nuber $Num1 is greater than Second Number $Num2"
-else
+elif [ $Num2 -gt $Num1]
+then 
 echo "Second  Number $Num2 is greater than First Number $Num1"
+else
+echo "both are equal"
 fi
